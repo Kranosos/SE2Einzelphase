216b45de638969fcb4574a6118f2b796045beb52
manue <manuel2.hoi@gmail.com>
2023-03-04 18:27:49 +0100

    4. Commit: implemented server connection

--- app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.se2einzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
 import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +23,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void sendMatrikelnummer(View v) throws InterruptedException {
+        TextView matrikelnummerFeld = (TextView) findViewById(R.id.number_input);
+
+        String matrikelnummer = String.valueOf(matrikelnummerFeld.getText());
+        ServerCommunication serverCommunication = new ServerCommunication("143.205.174.165", 53212, matrikelnummer);
+        Thread thread = new Thread(serverCommunication);
+        thread.start();
+        thread.join();
+
+
+        updateOutput(serverCommunication.response);
+
+    }
+
+    public void updateOutput(String matrikelnummer) {
+        TextView textView = findViewById(R.id.output);
+        textView.setSingleLine(false);
+        textView.setText(matrikelnummer);
 
     }
 }
---
+++ app/src/main/java/com/example/se2einzelphase/ServerCommunication.java
@@ -0,0 +1,49 @@
+package com.example.se2einzelphase;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+public class ServerCommunication implements Runnable{
+
+
+    String host;
+    int port;
+    String message;
+    boolean gotResponse;
+    String response;
+
+    public ServerCommunication(String host, int port, String message) {
+        this.host = host;
+        this.port = port;
+        this.message = message;
+        gotResponse = false;
+    }
+
+    @Override
+    public void run()  {
+
+        try {
+            Socket clientSocket;
+            clientSocket = new Socket(host, port);
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            outToServer.writeBytes(message + '\n');
+
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            response = inFromServer.readLine();
+            gotResponse = true;
+            inFromServer.close();
+            outToServer.close();
+            clientSocket.close();
+
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
